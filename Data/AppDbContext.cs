f0b5d907d15fd2780ce403f226a60e5419fe26b7
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-12-11 04:51:39 -0400

    Configure DbContext + appsettings.json

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+using System;
+using LendingApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LendingApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)  : base(options) {}
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Loan> Loans => Set<Loan>();
+    public DbSet<Payment> Payments => Set<Payment>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Loan>()
+            .Property(l => l.PrincipalAmount)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Loan>()
+            .Property(l => l.InterestRate)
+            .HasPrecision(3, 2);
+
+        modelBuilder.Entity<Payment>()
+            .Property(p => p.Amount)
+            .HasPrecision(18, 2);
+    }
+}
