cd76a52f87929bf03321a8798fae66609daf17b7
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-12-22 04:25:33 -0400

    Configure program.cs for JsonSerializerOptions with Add Scoped for Customers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using System.Text.Json.Serialization;
+using LendingApi.Application.Repositories;
+using LendingApi.Application.Services;
 using LendingApi.Data;
+using LendingApi.Data.SqlDatabase;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +13,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            return new BadRequestObjectResult(context.ModelState);
+        };
+    });
+
+builder.Services.AddScoped<ICustomerRepository, SqlCustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
